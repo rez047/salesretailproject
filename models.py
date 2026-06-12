f1d532a1a9d9ec411ad025b7ff64b189301c1dd3
rez047 <113913401+rez047@users.noreply.github.com>
2026-06-12 19:51:44 +0300

    Update models.py

--- models.py
+++ models.py
@@ -227,7 +227,9 @@ class Question(db.Model):
 
 
 class Notification(db.Model):
+    __tablename__ = "notification"
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     message = db.Column(db.String(255))
     is_read = db.Column(db.Boolean, default=False)
